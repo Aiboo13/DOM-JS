3146786b2e32b683592b50724b346127ccdce335
MINASSAHIDDIN <minassahiddin@gmail.com>
2025-02-01 21:04:18 +0700

    query selector

--- selection/app.js
+++ selection/app.js
@@ -3,7 +3,9 @@ const allImages = document.getElementsByTagName('img');
 
 // for (let img of allImages) {
 // 	console.log(img.src);
+
 // 	img.src =
+	// ini untuk merubah src 
 // 		'https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Cat_August_2010-4.jpg/1920px-Cat_August_2010-4.jpg';
 // }
 
@@ -12,14 +14,22 @@ const squareImages = document.getElementsByClassName('square');
 
 for (let img of squareImages) {
 	img.src =
+	// ini untuk merubah src 
 		'https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Cat_August_2010-4.jpg/1920px-Cat_August_2010-4.jpg';
 }
 
 const links = document.querySelectorAll('a');
+// memilih element angker 
+
+// mengedit style link
+
 
 for (let link of links) {
 	link.style.color = 'rgb(0, 108, 134)';
 	link.style.textDecorationColor = 'magenta';
 	link.style.textDecorationStyle = 'wavy';
 }
+
+ocument.querySelectorAll('p a')
+// cara membaca nya adalah carikan saya semua element a pada p
 
