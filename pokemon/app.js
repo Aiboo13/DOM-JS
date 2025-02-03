0d9aacc4002a69ff1a0c6be6459ddfa7d842b345
MINASSAHIDDIN <minassahiddin@gmail.com>
2025-02-03 22:59:00 +0700

    ketinggalan

--- pokemon/app.js
+++ pokemon/app.js
@@ -9,13 +9,21 @@ const baseImgURL = `https://raw.githubusercontent.com/PokeAPI/sprites/master/spr
 
 for (let i = 1; i <=1250; i++) {
 	const pokeball = document.createElement('div');
+	// membuat elemnt div 
 	const imgpokemon = document.createElement("img")
+	// membuat elemnt img
 	pokeball.classList.add('pokemon');
+	// menambahkan class dari style css
 	const label = document.createElement('span');
+	// membuat elemnt span
 	label.append(`#${i}`)
 	imgpokemon.src = `${baseImgURL}${i}.png`;
+	// make img inicial element
 	
 	pokeball.appendChild(imgpokemon);
+	// menyisipkan elemt imgpokemon in pokeball
 	pokeball.appendChild(label)
+	// menyiipkan label in pokeball
 	container.appendChild(pokeball);
+	// menyisipkan pokeball ke container
 }
