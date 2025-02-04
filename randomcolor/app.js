298bb1c082b9a2bb4490e9d11366dc61928f4c46
MINASSAHIDDIN <minassahiddin@gmail.com>
2025-02-04 17:35:07 +0700

    menambhakan warna pada text biar contras

--- randomcolor/app.js
+++ randomcolor/app.js
@@ -1,16 +1,28 @@
 const button = document.querySelector('button');
 const h1 = document.querySelector('h1');
+const h2 = document.querySelector('h2');
 
 button.addEventListener('click', () =>{
 	const newColor = generateRandomcolor();
+	const newcColorforh1 = generateForh1();
 	document.body.style.backgroundColor= newColor;
-	h1.innerText = newColor;
+	h1.style.color = newcColorforh1;
+	h1.innerText = newcColorforh1;
+	h2.innerText = newColor;
 })
 
 const generateRandomcolor = () => {
 	const r = Math.floor(Math.random() * 25	);
 	const g = Math.floor(Math.random() * 25	);
 	const b = Math.floor(Math.random() * 25	);
+
+	return `rgb(${r}, ${g}, ${b})`;
+}
+
+const generateForh1 = () => {
+	const r = Math.floor(Math.random() * 255);
+	const g = Math.floor(Math.random() * 255);
+	const b = Math.floor(Math.random() * 255);
 
 	return `rgb(${r}, ${g}, ${b})`;
 }
