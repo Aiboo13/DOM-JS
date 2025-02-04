e42e56ee7c7d92dd52ee878e00da4609873d41eb
MINASSAHIDDIN <minassahiddin@gmail.com>
2025-02-04 17:38:55 +0700

    no this

--- randomcolor/app.js
+++ randomcolor/app.js
@@ -1,28 +1,48 @@
-const button = document.querySelector('button');
-const h1 = document.querySelector('h1');
-const h2 = document.querySelector('h2');
+// const button = document.querySelector('button');
+// const h1 = document.querySelector('h1');
+// const h2 = document.querySelector('h2');
 
-button.addEventListener('click', () =>{
-	const newColor = generateRandomcolor();
-	const newcColorforh1 = generateForh1();
-	document.body.style.backgroundColor= newColor;
-	h1.style.color = newcColorforh1;
-	h1.innerText = newcColorforh1;
-	h2.innerText = newColor;
-})
+// button.addEventListener('click', () =>{
+// 	const newColor = generateRandomcolor();
+// 	const newcColorforh1 = generateForh1();
+// 	document.body.style.backgroundColor= newColor;
+// 	h1.style.color = newcColorforh1;
+// 	h1.innerText = newcColorforh1;
+// 	h2.innerText = newColor;
+// })
 
 const generateRandomcolor = () => {
-	const r = Math.floor(Math.random() * 25	);
-	const g = Math.floor(Math.random() * 25	);
-	const b = Math.floor(Math.random() * 25	);
+	const r = Math.floor(Math.random() * 255	);
+	const g = Math.floor(Math.random() * 255);
+	const b = Math.floor(Math.random() * 255	);
 
 	return `rgb(${r}, ${g}, ${b})`;
 }
 
-const generateForh1 = () => {
-	const r = Math.floor(Math.random() * 255);
-	const g = Math.floor(Math.random() * 255);
-	const b = Math.floor(Math.random() * 255);
+// const generateForh1 = () => {
+// 	const r = Math.floor(Math.random() * 255);
+// 	const g = Math.floor(Math.random() * 255);
+// 	const b = Math.floor(Math.random() * 255);
 
-	return `rgb(${r}, ${g}, ${b})`;
+// 	return `rgb(${r}, ${g}, ${b})`;
+// }
+
+
+const buttons = document.querySelectorAll('button');
+
+for (const button of buttons) {
+	button.addEventListener(`click`, ()=> {
+		button.style.backgroundColor = generateRandomcolor();
+		//jangan lupa titik style
+		button.style.color = generateRandomcolor();
+	})
+}
+const h1 = document.querySelectorAll('h1');
+
+for (const h1s of h1) {
+	h1s.addEventListener(`click`, ()=> {
+		h1s.style.backgroundColor = generateRandomcolor();
+		//jangan lupa titik style
+		h1s.style.color = generateRandomcolor();
+	})
 }
