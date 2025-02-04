6a1feb8e6aeb730a21ea07e9968645c1c6294e98
MINASSAHIDDIN <minassahiddin@gmail.com>
2025-02-04 22:09:12 +0700

    preventDefault suatu event mencegah action

--- eventobject/app.js
+++ eventobject/app.js
@@ -25,3 +25,11 @@ input.addEventListener(`keydown`,(e) => {
 			break;
 	}
 });
+
+
+const form = document.querySelector('#form');
+
+form.addEventListener('submit', (e) => {
+	e.preventDefault(); // menhentikan submit form secara default
+  console.log(`Form submitted`);
+});
