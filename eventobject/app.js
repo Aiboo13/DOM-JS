71d8350f43a0c43af8bc40ff22ed92395ae1f57c
MINASSAHIDDIN <minassahiddin@gmail.com>
2025-02-04 22:38:25 +0700

    input notes

--- eventobject/app.js
+++ eventobject/app.js
@@ -1,35 +1,44 @@
-const button = document.querySelector('button').addEventListener('click',(e) => {
-	console.log(e);
-});
+// const button = document.querySelector('button').addEventListener('click',(e) => {
+// 	console.log(e);
+// });
 
-const input = document.querySelector('input');
-input.addEventListener(`keydown`,(e) => {
-	switch (e.code) {
-		// e.code tanpa tanda petik
-		case `ArrowDown`:
-			// a besar setap kata yang di sambung diawali huruf besar 
-			console.log('bawah')
-			break;
-			case `ArrowLeft`:
-			console.log('kiri')
-			break;
-			case `ArrowRight`:
-			console.log('Kanan')
-			break;
-			case `ArrowUp`:
-			console.log('atas')
-			break;
+// // const input = document.querySelector('input');
+// input.addEventListener(`keydown`,(e) => {
+// 	switch (e.code) {
+// 		// e.code tanpa tanda petik
+// 		case `ArrowDown`:
+// 			// a besar setap kata yang di sambung diawali huruf besar 
+// 			console.log('bawah')
+// 			break;
+// 			case `ArrowLeft`:
+// 			console.log('kiri')
+// 			break;
+// 			case `ArrowRight`:
+// 			console.log('Kanan')
+// 			break;
+// 			case `ArrowUp`:
+// 			console.log('atas')
+// 			break;
 	
-		default:
-			console.log('tidak diketahui');
-			break;
-	}
-});
+// 		default:
+// 			console.log('tidak diketahui');
+// 			break;
+// 	}
+// });
 
 
 const form = document.querySelector('#form');
+const input = document.querySelector('input');
+const list = document.querySelector('#notes');
 
-form.addEventListener('submit', (e) => {
+form.addEventListener('submit', function(e){
 	e.preventDefault(); // menhentikan submit form secara default
-  console.log(`Form submitted`);
+  const noteValue = input.value;
+	const newList = document.createElement('li');
+	// membyat element baru yaitu list
+	newList.innerText = noteValue;
+	list.append(newList);
+	// menambhkan newlist di list
+	input.value = '';
+	// supaya kembali kosong setelah sudmid
 });
